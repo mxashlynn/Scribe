e8e5925b3e0d3fa9424b39fb85590070fde3587b
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-07-09 10:23:19 -0700

    Adds tab-based navigation.

--- Scribe/EditorForm.Designer.cs
+++ Scribe/EditorForm.Designer.cs
@@ -63,7 +63,11 @@ namespace Scribe
             this.DocumentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
             this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditorTabs = new System.Windows.Forms.TabControl();
+            this.InfoTabPage = new System.Windows.Forms.TabPage();
+            this.BeingsTabPage = new System.Windows.Forms.TabPage();
             this.MainMenuBar.SuspendLayout();
+            this.EditorTabs.SuspendLayout();
             this.SuspendLayout();
             // 
             // EditorStatusStrip
@@ -319,6 +323,39 @@ namespace Scribe
             this.AboutToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
             this.AboutToolStripMenuItem.Text = "&About...";
             // 
+            // EditorTabs
+            // 
+            this.EditorTabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EditorTabs.Controls.Add(this.InfoTabPage);
+            this.EditorTabs.Controls.Add(this.BeingsTabPage);
+            this.EditorTabs.Location = new System.Drawing.Point(12, 27);
+            this.EditorTabs.Name = "EditorTabs";
+            this.EditorTabs.SelectedIndex = 0;
+            this.EditorTabs.Size = new System.Drawing.Size(960, 696);
+            this.EditorTabs.TabIndex = 2;
+            // 
+            // InfoTabPage
+            // 
+            this.InfoTabPage.Location = new System.Drawing.Point(4, 22);
+            this.InfoTabPage.Name = "InfoTabPage";
+            this.InfoTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.InfoTabPage.Size = new System.Drawing.Size(952, 670);
+            this.InfoTabPage.TabIndex = 0;
+            this.InfoTabPage.Text = "Information";
+            this.InfoTabPage.UseVisualStyleBackColor = true;
+            // 
+            // BeingsTabPage
+            // 
+            this.BeingsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.BeingsTabPage.Name = "BeingsTabPage";
+            this.BeingsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.BeingsTabPage.Size = new System.Drawing.Size(952, 670);
+            this.BeingsTabPage.TabIndex = 1;
+            this.BeingsTabPage.Text = "Beings";
+            this.BeingsTabPage.UseVisualStyleBackColor = true;
+            // 
             // EditorForm
             // 
             this.AccessibleDescription = "The primary interactive editor window.";
@@ -327,6 +364,7 @@ namespace Scribe
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.EditorTabs);
             this.Controls.Add(this.EditorStatusStrip);
             this.Controls.Add(this.MainMenuBar);
             this.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -338,6 +376,7 @@ namespace Scribe
             this.Text = "Scribe";
             this.MainMenuBar.ResumeLayout(false);
             this.MainMenuBar.PerformLayout();
+            this.EditorTabs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -377,6 +416,9 @@ namespace Scribe
         private System.Windows.Forms.ToolStripMenuItem ListMaxIDsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ListTagsToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator ToolStripSeparator5;
+        private System.Windows.Forms.TabControl EditorTabs;
+        private System.Windows.Forms.TabPage InfoTabPage;
+        private System.Windows.Forms.TabPage BeingsTabPage;
     }
 }
 
