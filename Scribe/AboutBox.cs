c4af052fb0038eb370bb4d5624810da7a7da1a48
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-07-17 09:37:13 -0700

    Improvements to code and comments in AboutBox.cs

--- Scribe/AboutBox.cs
+++ Scribe/AboutBox.cs
@@ -6,6 +6,9 @@ namespace Scribe
 {
     partial class AboutBox : Form
     {
+        /// <summary>
+        /// A modal dialogue that present descriptive information about the application and library.
+        /// </summary>
         public AboutBox()
         {
             InitializeComponent();
@@ -19,15 +22,17 @@ namespace Scribe
         }
 
         #region Assembly Attribute Accessors
-
+        /// <summary>
+        /// The name of the assembly.
+        /// </summary>
         public string AssemblyTitle
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
                 if (attributes.Length > 0)
                 {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    var titleAttribute = (AssemblyTitleAttribute)attributes[0];
                     if (titleAttribute.Title != "")
                     {
                         return titleAttribute.Title;
@@ -37,55 +42,59 @@ namespace Scribe
             }
         }
 
+        /// <summary>
+        /// What the purpose of the assembly.
+        /// </summary>
         public string AssemblyDescription
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                return attributes.Length == 0
+                    ? ""
+                    : ((AssemblyDescriptionAttribute)attributes[0]).Description;
             }
         }
 
+        /// <summary>
+        /// The name of the product.
+        /// </summary>
         public string AssemblyProduct
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                return attributes.Length == 0
+                    ? ""
+                    : ((AssemblyProductAttribute)attributes[0]).Product;
             }
         }
 
+        /// <summary>
+        /// Copyright information.
+        /// </summary>
         public string AssemblyCopyright
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                return attributes.Length == 0
+                    ? ""
+                    : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
             }
         }
 
+        /// <summary>
+        /// The name of the group that created the product.
+        /// </summary>
         public string AssemblyCompany
         {
             get
             {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+                var attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                return attributes.Length == 0
+                    ? ""
+                    : ((AssemblyCompanyAttribute)attributes[0]).Company;
             }
         }
         #endregion
