0eee0bec78a0515febbbdf44b8c01995dcd70ea4
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-07-17 13:35:02 -0700

    Lays out an AddRecipeElementBox modal dialogue.

---
+++ Scribe/AddRecipeElementBox.Designer.cs
@@ -0,0 +1,153 @@
+namespace Scribe
+{
+    partial class AddRecipeElementBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.CancelButtonControl = new System.Windows.Forms.Button();
+            this.OkayButton = new System.Windows.Forms.Button();
+            this.ElementTagLabel = new System.Windows.Forms.Label();
+            this.ElementTagTextBox = new System.Windows.Forms.TextBox();
+            this.ElementAmountLabel = new System.Windows.Forms.Label();
+            this.ElementAmountTextBox = new System.Windows.Forms.TextBox();
+            this.TableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TableLayoutPanel
+            // 
+            this.TableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TableLayoutPanel.ColumnCount = 2;
+            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+            this.TableLayoutPanel.Controls.Add(this.CancelButtonControl, 0, 2);
+            this.TableLayoutPanel.Controls.Add(this.OkayButton, 1, 2);
+            this.TableLayoutPanel.Controls.Add(this.ElementTagLabel, 0, 0);
+            this.TableLayoutPanel.Controls.Add(this.ElementTagTextBox, 1, 0);
+            this.TableLayoutPanel.Controls.Add(this.ElementAmountLabel, 0, 1);
+            this.TableLayoutPanel.Controls.Add(this.ElementAmountTextBox, 1, 1);
+            this.TableLayoutPanel.Location = new System.Drawing.Point(13, 13);
+            this.TableLayoutPanel.Name = "TableLayoutPanel";
+            this.TableLayoutPanel.RowCount = 3;
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.Size = new System.Drawing.Size(258, 135);
+            this.TableLayoutPanel.TabIndex = 1;
+            // 
+            // CancelButtonControl
+            // 
+            this.CancelButtonControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelButtonControl.Location = new System.Drawing.Point(7, 109);
+            this.CancelButtonControl.Name = "CancelButtonControl";
+            this.CancelButtonControl.Size = new System.Drawing.Size(75, 23);
+            this.CancelButtonControl.TabIndex = 8;
+            this.CancelButtonControl.Text = "&Cancel";
+            this.CancelButtonControl.UseVisualStyleBackColor = true;
+            // 
+            // OkayButton
+            // 
+            this.OkayButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OkayButton.Location = new System.Drawing.Point(180, 109);
+            this.OkayButton.Name = "OkayButton";
+            this.OkayButton.Size = new System.Drawing.Size(75, 23);
+            this.OkayButton.TabIndex = 3;
+            this.OkayButton.Text = "&OK";
+            this.OkayButton.UseVisualStyleBackColor = true;
+            // 
+            // ElementTagLabel
+            // 
+            this.ElementTagLabel.AutoSize = true;
+            this.ElementTagLabel.Location = new System.Drawing.Point(3, 0);
+            this.ElementTagLabel.Name = "ElementTagLabel";
+            this.ElementTagLabel.Size = new System.Drawing.Size(52, 15);
+            this.ElementTagLabel.TabIndex = 4;
+            this.ElementTagLabel.Text = "New Tag";
+            // 
+            // ElementTagTextBox
+            // 
+            this.ElementTagTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ElementTagTextBox.Location = new System.Drawing.Point(88, 3);
+            this.ElementTagTextBox.Multiline = true;
+            this.ElementTagTextBox.Name = "ElementTagTextBox";
+            this.ElementTagTextBox.Size = new System.Drawing.Size(167, 69);
+            this.ElementTagTextBox.TabIndex = 5;
+            // 
+            // ElementAmountLabel
+            // 
+            this.ElementAmountLabel.AutoSize = true;
+            this.ElementAmountLabel.Location = new System.Drawing.Point(3, 75);
+            this.ElementAmountLabel.Name = "ElementAmountLabel";
+            this.ElementAmountLabel.Size = new System.Drawing.Size(51, 15);
+            this.ElementAmountLabel.TabIndex = 6;
+            this.ElementAmountLabel.Text = "Amount";
+            // 
+            // ElementAmountTextBox
+            // 
+            this.ElementAmountTextBox.Location = new System.Drawing.Point(88, 78);
+            this.ElementAmountTextBox.Name = "ElementAmountTextBox";
+            this.ElementAmountTextBox.Size = new System.Drawing.Size(100, 23);
+            this.ElementAmountTextBox.TabIndex = 7;
+            // 
+            // AddRecipeElementBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelButtonControl;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.TableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "AddRecipeElementBox";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Recipe Element";
+            this.TableLayoutPanel.ResumeLayout(false);
+            this.TableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TableLayoutPanel;
+        private System.Windows.Forms.Button OkayButton;
+        private System.Windows.Forms.Button CancelButtonControl;
+        private System.Windows.Forms.Label ElementTagLabel;
+        private System.Windows.Forms.TextBox ElementTagTextBox;
+        private System.Windows.Forms.Label ElementAmountLabel;
+        private System.Windows.Forms.TextBox ElementAmountTextBox;
+    }
+}
---
+++ Scribe/AddRecipeElementBox.cs
@@ -0,0 +1,16 @@
+using System.Windows.Forms;
+
+namespace Scribe
+{
+    /// <summary>
+    /// A modal dialogue that enables the user to add a new <see cref="RecipeElement"/> to a collection of recipe elements.
+    /// </summary>
+    partial class AddRecipeElementBox : Form
+    {
+        /// <summary>
+        /// Initialize a new <see cref="AddRecipeElementBox"/>.
+        /// </summary>
+        public AddRecipeElementBox()
+            => InitializeComponent();
+    }
+}
