54953b4d93528c45d768fc903a5934685db188c4
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-08-10 18:01:22 -0700

    Adds controls for known options.

--- Scribe/OptionsBox.Designer.cs
+++ Scribe/OptionsBox.Designer.cs
@@ -32,9 +32,21 @@ namespace Scribe
             this.LabelTheme = new System.Windows.Forms.Label();
             this.PanelEditorTheme = new System.Windows.Forms.Panel();
             this.RadioButtonColorfulTheme = new System.Windows.Forms.RadioButton();
-            this.RadioButtonLightTheme = new System.Windows.Forms.RadioButton();
+            this.RadioButtonOSTheme = new System.Windows.Forms.RadioButton();
+            this.LabelSuggestStoryIDs = new System.Windows.Forms.Label();
+            this.CheckBoxSuggestStoryIDs = new System.Windows.Forms.CheckBox();
+            this.LabelAutoSaveInterval = new System.Windows.Forms.Label();
+            this.TextBoxAutoSaveInterval = new System.Windows.Forms.TextBox();
+            this.LabelAutoSaveExplanation = new System.Windows.Forms.Label();
+            this.LabelDefaultFolder = new System.Windows.Forms.Label();
+            this.RadioButtonDesktop = new System.Windows.Forms.RadioButton();
+            this.RadioButtonDocuments = new System.Windows.Forms.RadioButton();
+            this.PanelDefaultFolder = new System.Windows.Forms.Panel();
+            this.LabelFlavorFilter = new System.Windows.Forms.Label();
+            this.CheckBoxFlavorFilters = new System.Windows.Forms.CheckBox();
             this.TableLayoutPanel.SuspendLayout();
             this.PanelEditorTheme.SuspendLayout();
+            this.PanelDefaultFolder.SuspendLayout();
             this.SuspendLayout();
             // 
             // TableLayoutPanel
@@ -45,20 +57,30 @@ namespace Scribe
             this.TableLayoutPanel.ColumnCount = 2;
             this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
             this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
-            this.TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.TableLayoutPanel.Controls.Add(this.OkayButton, 1, 5);
+            this.TableLayoutPanel.Controls.Add(this.PanelDefaultFolder, 1, 5);
             this.TableLayoutPanel.Controls.Add(this.LabelTheme, 0, 0);
             this.TableLayoutPanel.Controls.Add(this.PanelEditorTheme, 1, 0);
+            this.TableLayoutPanel.Controls.Add(this.LabelSuggestStoryIDs, 0, 2);
+            this.TableLayoutPanel.Controls.Add(this.CheckBoxSuggestStoryIDs, 1, 2);
+            this.TableLayoutPanel.Controls.Add(this.LabelAutoSaveInterval, 0, 3);
+            this.TableLayoutPanel.Controls.Add(this.TextBoxAutoSaveInterval, 1, 3);
+            this.TableLayoutPanel.Controls.Add(this.LabelAutoSaveExplanation, 1, 4);
+            this.TableLayoutPanel.Controls.Add(this.LabelDefaultFolder, 0, 5);
+            this.TableLayoutPanel.Controls.Add(this.OkayButton, 1, 7);
+            this.TableLayoutPanel.Controls.Add(this.LabelFlavorFilter, 0, 1);
+            this.TableLayoutPanel.Controls.Add(this.CheckBoxFlavorFilters, 1, 1);
             this.TableLayoutPanel.Location = new System.Drawing.Point(10, 10);
             this.TableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.TableLayoutPanel.Name = "TableLayoutPanel";
-            this.TableLayoutPanel.RowCount = 6;
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.TableLayoutPanel.RowCount = 8;
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.TableLayoutPanel.Size = new System.Drawing.Size(486, 306);
             this.TableLayoutPanel.TabIndex = 0;
             // 
@@ -66,10 +88,10 @@ namespace Scribe
             // 
             this.OkayButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.OkayButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.OkayButton.Location = new System.Drawing.Point(394, 276);
+            this.OkayButton.Location = new System.Drawing.Point(394, 281);
             this.OkayButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.OkayButton.Name = "OkayButton";
-            this.OkayButton.Size = new System.Drawing.Size(88, 27);
+            this.OkayButton.Size = new System.Drawing.Size(88, 22);
             this.OkayButton.TabIndex = 24;
             this.OkayButton.Text = "&OK";
             // 
@@ -91,7 +113,7 @@ namespace Scribe
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PanelEditorTheme.Controls.Add(this.RadioButtonColorfulTheme);
-            this.PanelEditorTheme.Controls.Add(this.RadioButtonLightTheme);
+            this.PanelEditorTheme.Controls.Add(this.RadioButtonOSTheme);
             this.PanelEditorTheme.Location = new System.Drawing.Point(163, 3);
             this.PanelEditorTheme.Name = "PanelEditorTheme";
             this.PanelEditorTheme.Size = new System.Drawing.Size(320, 24);
@@ -103,21 +125,130 @@ namespace Scribe
             this.RadioButtonColorfulTheme.Checked = true;
             this.RadioButtonColorfulTheme.Location = new System.Drawing.Point(0, 5);
             this.RadioButtonColorfulTheme.Name = "RadioButtonColorfulTheme";
-            this.RadioButtonColorfulTheme.Size = new System.Drawing.Size(75, 19);
+            this.RadioButtonColorfulTheme.Size = new System.Drawing.Size(68, 19);
             this.RadioButtonColorfulTheme.TabIndex = 1;
             this.RadioButtonColorfulTheme.TabStop = true;
-            this.RadioButtonColorfulTheme.Text = "Colourful";
+            this.RadioButtonColorfulTheme.Text = "Colorful";
             this.RadioButtonColorfulTheme.UseVisualStyleBackColor = true;
             // 
-            // RadioButtonLightTheme
+            // RadioButtonOSTheme
             // 
-            this.RadioButtonLightTheme.AutoSize = true;
-            this.RadioButtonLightTheme.Location = new System.Drawing.Point(100, 5);
-            this.RadioButtonLightTheme.Name = "RadioButtonLightTheme";
-            this.RadioButtonLightTheme.Size = new System.Drawing.Size(52, 19);
-            this.RadioButtonLightTheme.TabIndex = 2;
-            this.RadioButtonLightTheme.Text = "Light";
-            this.RadioButtonLightTheme.UseVisualStyleBackColor = true;
+            this.RadioButtonOSTheme.AutoSize = true;
+            this.RadioButtonOSTheme.Location = new System.Drawing.Point(100, 5);
+            this.RadioButtonOSTheme.Name = "RadioButtonOSTheme";
+            this.RadioButtonOSTheme.Size = new System.Drawing.Size(81, 19);
+            this.RadioButtonOSTheme.TabIndex = 2;
+            this.RadioButtonOSTheme.Text = "OS Default";
+            this.RadioButtonOSTheme.UseVisualStyleBackColor = true;
+            // 
+            // LabelSuggestStoryIDs
+            // 
+            this.LabelSuggestStoryIDs.AutoSize = true;
+            this.LabelSuggestStoryIDs.Location = new System.Drawing.Point(3, 60);
+            this.LabelSuggestStoryIDs.Name = "LabelSuggestStoryIDs";
+            this.LabelSuggestStoryIDs.Size = new System.Drawing.Size(98, 15);
+            this.LabelSuggestStoryIDs.TabIndex = 29;
+            this.LabelSuggestStoryIDs.Text = "Suggest Story IDs";
+            // 
+            // CheckBoxSuggestStoryIDs
+            // 
+            this.CheckBoxSuggestStoryIDs.AutoSize = true;
+            this.CheckBoxSuggestStoryIDs.Checked = true;
+            this.CheckBoxSuggestStoryIDs.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBoxSuggestStoryIDs.Location = new System.Drawing.Point(163, 63);
+            this.CheckBoxSuggestStoryIDs.Name = "CheckBoxSuggestStoryIDs";
+            this.CheckBoxSuggestStoryIDs.Size = new System.Drawing.Size(15, 14);
+            this.CheckBoxSuggestStoryIDs.TabIndex = 30;
+            this.CheckBoxSuggestStoryIDs.UseVisualStyleBackColor = true;
+            // 
+            // LabelAutoSaveInterval
+            // 
+            this.LabelAutoSaveInterval.AutoSize = true;
+            this.LabelAutoSaveInterval.Location = new System.Drawing.Point(3, 90);
+            this.LabelAutoSaveInterval.Name = "LabelAutoSaveInterval";
+            this.LabelAutoSaveInterval.Size = new System.Drawing.Size(151, 15);
+            this.LabelAutoSaveInterval.TabIndex = 31;
+            this.LabelAutoSaveInterval.Text = "Interval Between Autosaves";
+            // 
+            // TextBoxAutoSaveInterval
+            // 
+            this.TextBoxAutoSaveInterval.Location = new System.Drawing.Point(163, 93);
+            this.TextBoxAutoSaveInterval.Name = "TextBoxAutoSaveInterval";
+            this.TextBoxAutoSaveInterval.Size = new System.Drawing.Size(68, 23);
+            this.TextBoxAutoSaveInterval.TabIndex = 32;
+            this.TextBoxAutoSaveInterval.Text = "5";
+            // 
+            // LabelAutoSaveExplanation
+            // 
+            this.LabelAutoSaveExplanation.AutoSize = true;
+            this.LabelAutoSaveExplanation.Location = new System.Drawing.Point(163, 120);
+            this.LabelAutoSaveExplanation.Name = "LabelAutoSaveExplanation";
+            this.LabelAutoSaveExplanation.Size = new System.Drawing.Size(214, 15);
+            this.LabelAutoSaveExplanation.TabIndex = 33;
+            this.LabelAutoSaveExplanation.Text = "Interval is in minutes.  0 = No autosave.";
+            // 
+            // LabelDefaultFolder
+            // 
+            this.LabelDefaultFolder.AutoSize = true;
+            this.LabelDefaultFolder.Location = new System.Drawing.Point(3, 150);
+            this.LabelDefaultFolder.Name = "LabelDefaultFolder";
+            this.LabelDefaultFolder.Size = new System.Drawing.Size(81, 15);
+            this.LabelDefaultFolder.TabIndex = 34;
+            this.LabelDefaultFolder.Text = "Default Folder";
+            // 
+            // RadioButtonDesktop
+            // 
+            this.RadioButtonDesktop.AutoSize = true;
+            this.RadioButtonDesktop.Checked = true;
+            this.RadioButtonDesktop.Location = new System.Drawing.Point(0, 5);
+            this.RadioButtonDesktop.Name = "RadioButtonDesktop";
+            this.RadioButtonDesktop.Size = new System.Drawing.Size(68, 19);
+            this.RadioButtonDesktop.TabIndex = 1;
+            this.RadioButtonDesktop.TabStop = true;
+            this.RadioButtonDesktop.Text = "Desktop";
+            this.RadioButtonDesktop.UseVisualStyleBackColor = true;
+            // 
+            // RadioButtonDocuments
+            // 
+            this.RadioButtonDocuments.AutoSize = true;
+            this.RadioButtonDocuments.Location = new System.Drawing.Point(100, 5);
+            this.RadioButtonDocuments.Name = "RadioButtonDocuments";
+            this.RadioButtonDocuments.Size = new System.Drawing.Size(86, 19);
+            this.RadioButtonDocuments.TabIndex = 2;
+            this.RadioButtonDocuments.Text = "Documents";
+            this.RadioButtonDocuments.UseVisualStyleBackColor = true;
+            // 
+            // PanelDefaultFolder
+            // 
+            this.PanelDefaultFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PanelDefaultFolder.Controls.Add(this.RadioButtonDesktop);
+            this.PanelDefaultFolder.Controls.Add(this.RadioButtonDocuments);
+            this.PanelDefaultFolder.Location = new System.Drawing.Point(163, 153);
+            this.PanelDefaultFolder.Name = "PanelDefaultFolder";
+            this.PanelDefaultFolder.Size = new System.Drawing.Size(320, 24);
+            this.PanelDefaultFolder.TabIndex = 28;
+            // 
+            // LabelFlavorFilter
+            // 
+            this.LabelFlavorFilter.AutoSize = true;
+            this.LabelFlavorFilter.Location = new System.Drawing.Point(3, 30);
+            this.LabelFlavorFilter.Name = "LabelFlavorFilter";
+            this.LabelFlavorFilter.Size = new System.Drawing.Size(105, 15);
+            this.LabelFlavorFilter.TabIndex = 35;
+            this.LabelFlavorFilter.Text = "Show Flavor Filters";
+            // 
+            // CheckBoxFlavorFilters
+            // 
+            this.CheckBoxFlavorFilters.AutoSize = true;
+            this.CheckBoxFlavorFilters.Checked = true;
+            this.CheckBoxFlavorFilters.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBoxFlavorFilters.Location = new System.Drawing.Point(163, 33);
+            this.CheckBoxFlavorFilters.Name = "CheckBoxFlavorFilters";
+            this.CheckBoxFlavorFilters.Size = new System.Drawing.Size(15, 14);
+            this.CheckBoxFlavorFilters.TabIndex = 36;
+            this.CheckBoxFlavorFilters.UseVisualStyleBackColor = true;
             // 
             // OptionsBox
             // 
@@ -140,6 +271,8 @@ namespace Scribe
             this.TableLayoutPanel.PerformLayout();
             this.PanelEditorTheme.ResumeLayout(false);
             this.PanelEditorTheme.PerformLayout();
+            this.PanelDefaultFolder.ResumeLayout(false);
+            this.PanelDefaultFolder.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -151,6 +284,17 @@ namespace Scribe
         private System.Windows.Forms.Label LabelTheme;
         private System.Windows.Forms.Panel PanelEditorTheme;
         private System.Windows.Forms.RadioButton RadioButtonColorfulTheme;
-        private System.Windows.Forms.RadioButton RadioButtonLightTheme;
+        private System.Windows.Forms.RadioButton RadioButtonOSTheme;
+        private System.Windows.Forms.Panel PanelDefaultFolder;
+        private System.Windows.Forms.RadioButton RadioButtonDesktop;
+        private System.Windows.Forms.RadioButton RadioButtonDocuments;
+        private System.Windows.Forms.Label LabelSuggestStoryIDs;
+        private System.Windows.Forms.CheckBox CheckBoxSuggestStoryIDs;
+        private System.Windows.Forms.Label LabelAutoSaveInterval;
+        private System.Windows.Forms.TextBox TextBoxAutoSaveInterval;
+        private System.Windows.Forms.Label LabelAutoSaveExplanation;
+        private System.Windows.Forms.Label LabelDefaultFolder;
+        private System.Windows.Forms.Label LabelFlavorFilter;
+        private System.Windows.Forms.CheckBox CheckBoxFlavorFilters;
     }
 }
