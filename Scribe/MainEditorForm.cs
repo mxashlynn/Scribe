64052c50830cd90a90b9d3d0fbf7dd4d57796aaa
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-08-21 14:50:14 -0700

    Updates to the latest Parquet library.

--- Scribe/MainEditorForm.cs
+++ Scribe/MainEditorForm.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Media;
-using System.Runtime.InteropServices.ComTypes;
 using System.Windows.Forms;
 using ParquetClassLibrary;
 using ParquetClassLibrary.Beings;
