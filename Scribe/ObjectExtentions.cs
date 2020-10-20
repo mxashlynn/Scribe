141a0e55c90cc80decf4e274e16e29621229b3cb
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-10-19 17:26:06 -0700

    Fixes typo.

--- Scribe/ObjectExtentions.cs
+++ Scribe/ObjectExtentions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 namespace Scribe
 {
     /// <summary>
-    /// Extension methods for <see cref="Dictionary"/>.
+    /// Extension methods for <see cref="object"/>.
     /// </summary>
     public static class ObjectExtentions
     {
