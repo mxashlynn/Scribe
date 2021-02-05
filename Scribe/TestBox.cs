a5da59c135058182107bf33abcf1a9fec8e94b88
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-04 20:51:55 -0800

    Loads all test patterns into a single LayeredPixelBox test control.

--- Scribe/TestBox.cs
+++ Scribe/TestBox.cs
@@ -5,6 +5,15 @@ namespace Scribe
     partial class TestBox : Form
     {
         public TestBox()
-            => InitializeComponent();
+        {
+            InitializeComponent();
+
+            // Dynamically load the test patterns.
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(TestBox));
+            TestLayeredPixelBox1.ImageLayers[0] = (System.Drawing.Image)resources.GetObject("TestLayeredPixelBox1.Image");
+            TestLayeredPixelBox1.ImageLayers[1] = (System.Drawing.Image)resources.GetObject("TestLayeredPixelBox2.Image");
+            TestLayeredPixelBox1.ImageLayers[2] = (System.Drawing.Image)resources.GetObject("TestLayeredPixelBox3.Image");
+            TestLayeredPixelBox1.ImageLayers[3] = (System.Drawing.Image)resources.GetObject("TestLayeredPixelBox4.Image");
+        }
     }
 }
