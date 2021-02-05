5d4cfac963b4b3cab0dd62c4857c3cdaf4bf5ad4
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-05 13:43:36 -0800

    Adds map initialization routine.

--- Scribe/Forms/Development/TestMapGridForm.cs
+++ Scribe/Forms/Development/TestMapGridForm.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -8,11 +9,21 @@ namespace Scribe.Forms.Development
         /// <summary>The resolution at which the final map will be rendered.</summary>
         public static readonly Size TargetResolution = new Size(1280, 720);
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="TestMapGridForm"/>.
+        /// </summary>
         public TestMapGridForm()
-        {
-            InitializeComponent();
+            => InitializeComponent();
 
-            //ClientSize = TargetResolution;
+        /// <summary>
+        /// Sets up the map.
+        /// </summary>
+        /// <param name="EventData">Handled by parent.</param>
+        protected override void OnLoad(EventArgs EventData)
+        {
+            base.OnLoad(EventData);
+
+            // TODO Map stuff goes here!
         }
     }
 }
