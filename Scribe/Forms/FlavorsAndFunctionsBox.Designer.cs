abbf90d582a285859c000d45660f59edb4f5aa2c
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-17 18:54:23 -0800

    Adds flavor editor form template.

---
+++ Scribe/Forms/FlavorsAndFunctionsBox.Designer.cs
@@ -0,0 +1,54 @@
+
+namespace Scribe.Forms
+{
+    partial class FlavorsAndFunctions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TagsAndFlavorsBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 327);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TagsAndFlavorsBox";
+            this.Padding = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "TagsAndFlavorsBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Scribe/Forms/FlavorsAndFunctionsBox.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Scribe.Forms
+{
+    partial class FlavorsAndFunctions : Form
+    {
+        public FlavorsAndFunctions()
+        {
+            InitializeComponent();
+            Text = $"TODO: Functions and flavors!";
+        }
+    }
+}
