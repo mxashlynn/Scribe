1861234ca8125cbeb4a947d223e012f6c7efc7ec
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-28 13:52:55 -0800

    Removes duplicate method.

--- Scribe/Forms/Development/TestMapGridForm.Designer.cs
+++ Scribe/Forms/Development/TestMapGridForm.Designer.cs
@@ -7,18 +7,6 @@ namespace Scribe.Forms.Development
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         #region Windows Form Designer generated code
 
         /// <summary>
