8915e5dc499f591566af3abda6ee72fd913fc071
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-03-04 18:20:09 -0800

    Stubs in the world layout tool's form.

---
+++ Scribe/Forms/WorldLayoutForm.cs
@@ -0,0 +1,17 @@
+using System.Windows.Forms;
+using Parquet.Regions;
+
+namespace Scribe.Forms
+{
+    /// <summary>
+    /// A form that enables the user to organize ever <see cref="RegionModel"/> in the game.
+    /// </summary>
+    public partial class WorldLayoutForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorldLayoutForm"/> class.
+        /// </summary>
+        public WorldLayoutForm()
+            => InitializeComponent();
+    }
+}
