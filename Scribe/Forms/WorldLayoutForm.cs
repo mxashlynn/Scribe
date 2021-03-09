19333e4d195d078d5d1192c8cb76381d5b77f4ff
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-03-08 17:50:24 -0800

    Dynamically populates layout table.

--- Scribe/Forms/WorldLayoutForm.cs
+++ Scribe/Forms/WorldLayoutForm.cs
@@ -8,10 +8,42 @@ namespace Scribe.Forms
     /// </summary>
     public partial class WorldLayoutForm : Form
     {
+        #region Initialization
         /// <summary>
         /// Initializes a new instance of the <see cref="WorldLayoutForm"/> class.
         /// </summary>
         public WorldLayoutForm()
-            => InitializeComponent();
+        {
+            InitializeComponent();
+
+            #region Add Region Statics to Table
+            WorldLayoutTableLayoutPanel.SuspendLayout();
+
+            for (var column = 0; column < WorldLayoutTableLayoutPanel.ColumnCount; column++)
+            {
+                for (var row = 0; row < WorldLayoutTableLayoutPanel.RowCount; row++)
+                {
+                    var regionStatic = new Label
+                    {
+                        BackColor = SystemColors.AppWorkspace,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Font = new Font("Segoe UI", 9f, FontStyle.Regular, GraphicsUnit.Point),
+                        Location = new Point(0, 0),
+                        Margin = new Padding(0),
+                        Name = $"RegionStatic{column:00}_{row:00}",
+                        Size = new Size(25, 25),
+                        Tag = new Point2D(column, row),
+                        Text = $"{row:00}",
+                        TextAlign = ContentAlignment.MiddleCenter,
+                    };
+                    regionStatic.Click += new EventHandler(RegionStatic_Click);
+                    WorldLayoutTableLayoutPanel.Controls.Add(regionStatic, column, row);
+                }
+            }
+
+            WorldLayoutTableLayoutPanel.ResumeLayout(false);
+            #endregion
+        }
+        #endregion
     }
 }
