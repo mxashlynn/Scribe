ac38c98b2b52078e1eb8e9c5428bb237e9934db8
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-03-27 22:15:56 -0700

    Creates custom control to better display regions.

---
+++ ParquetCustomControls/DoubleBufferedStatic.Designer.cs
@@ -0,0 +1,51 @@
+namespace ParquetCustomControls
+{
+    partial class DoubleBufferedStatic
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DoubleBufferedStatic
+            // 
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "DoubleBufferedStatic";
+            this.Padding = new System.Windows.Forms.Padding(0);
+            this.Size = new System.Drawing.Size(25, 25);
+            this.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ParquetCustomControls/DoubleBufferedStatic.cs
@@ -0,0 +1,16 @@
+using System.Windows.Forms;
+
+namespace ParquetCustomControls
+{
+    /// <summary>
+    /// A control that can display text and color rapidly and without flickering.
+    /// </summary>
+    public partial class DoubleBufferedStatic : Label
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleBufferedStatic"/> class.
+        /// </summary>
+        public DoubleBufferedStatic()
+            => InitializeComponent();
+    }
+}
