586b25fde4abce3709df256c72d69c44944cd584
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-04-02 19:18:53 -0700

    Uses MapGridTestForm as a starting place for MapEditorForm.

--- Scribe/Forms/MapEditorForm.Designer.cs
+++ Scribe/Forms/MapEditorForm.Designer.cs
@@ -1,4 +1,3 @@
-﻿
 namespace Scribe.Forms
 {
     partial class MapEditorForm
@@ -8,33 +7,47 @@ namespace Scribe.Forms
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         #region Windows Form Designer generated code
-
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapEditorForm));
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MapEditorForm";
-        }
+            this.MapGrid = new ParquetCustomControls.MapPictureGrid();
+            this.SuspendLayout();
 
+            // 
+            // MapGrid
+            // 
+            this.MapGrid.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MapGrid.BackColor = System.Drawing.Color.Lavender;
+            this.MapGrid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.MapGrid.Controller = this;
+            this.MapGrid.Location = new System.Drawing.Point(10, 10);
+            this.MapGrid.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MapGrid.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.MapGrid.Name = "MapGrid";
+            this.MapGrid.Size = new System.Drawing.Size(1280, 720);
+            this.MapGrid.TabIndex = 0;
+            // 
+            // MapEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 740);
+            this.Controls.Add(this.MapGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MapEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Map Editor Form";
+            this.ResumeLayout(false);
+        }
         #endregion
+
+        private ParquetCustomControls.MapPictureGrid MapGrid;
     }
 }
--- Scribe/Forms/MapEditorForm.cs
+++ Scribe/Forms/MapEditorForm.cs
@@ -1,17 +1,114 @@
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 using Parquet.Regions;
+using ParquetCustomControls;
 
 namespace Scribe.Forms
 {
     /// <summary>
     /// A form that enables the user to edit a given <see cref="RegionStatus"/>.
     /// </summary>
-    public partial class MapEditorForm : Form
+    public partial class MapEditorForm : Form, IMapController
     {
+        #region Class Defaults
+        /// <summary>How often, in milliseconds, to redraw the map.</summary>
+        private const int RefreshInterval = 30;
+        #endregion
+
+        #region Characteristics
+        /// <summary>Periodically causes the map to redraw, similar to Draw events in a game loop.</summary>
+        private readonly Timer RefreshMapTimer = new Timer();
+        #endregion
+
+        #region Optimizations
+        /// <summary>Encapsulates the information needed when creating a control.</summary>
+        /// <remarks>In this instance, composited mode is turned on to improve frame rate.</remarks>
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+        #endregion
+
+        #region Initialization
         /// <summary>
         /// Initializes a new instance of the <see cref="MapEditorForm"/> class.
         /// </summary>
         public MapEditorForm()
-            => InitializeComponent();
+        {
+            InitializeComponent();
+            MapGrid.Controller = this;
+
+            RefreshMapTimer.Tick += (object sender, EventArgs arguments) =>
+            {
+                MapGrid.Refresh();
+                Text = MapGrid.Text;
+            };
+            RefreshMapTimer.Interval = RefreshInterval;
+        }
+
+        /// <summary>
+        /// Sets up the map.
+        /// </summary>
+        /// <param name="EventData">Handled by parent.</param>
+        protected override void OnLoad(EventArgs EventData)
+        {
+            base.OnLoad(EventData);
+
+            // Load Test Patterns
+            MapGrid.ImageByID[1] = (Bitmap)Image.FromFile("../../../Resources/ParquetTest1.png");
+            MapGrid.ImageByID[2] = (Bitmap)Image.FromFile("../../../Resources/ParquetTest2.png");
+            MapGrid.ImageByID[3] = (Bitmap)Image.FromFile("../../../Resources/ParquetTest3.png");
+            MapGrid.ImageByID[4] = (Bitmap)Image.FromFile("../../../Resources/ParquetTest4.png");
+
+            RefreshMapTimer.Start();
+        }
+        #endregion
+
+        #region Disposal
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> if managed resources should be disposed; otherwise, <c>false</c>.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            RefreshMapTimer.Dispose();
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region IMapController Implementation
+        /// <summary>
+        /// Occurs when the mouse hovers over a particular cell on the <see cref="MapPictureGrid"/>.
+        /// </summary>
+        /// <param name="inSender">The <see cref="MapPictureGrid"/> being hovered over.</param>
+        /// <param name="inEventArguments">Arguments from the original event.</param>
+        /// <param name="gridRestingLocation">The grid coordinates of the cell being hovered over.</param>
+        /// <param name="screenRestingLocation">The screen coordinates of the cell being hovered over.</param>
+        public void MapHover(object inSender, EventArgs inEventArguments, Point gridRestingLocation, Point screenRestingLocation)
+        {
+            var pixelRestingLocation = PointToClient(screenRestingLocation);
+            _ = MessageBox.Show($"Hovering over ({gridRestingLocation.X}, {gridRestingLocation.Y}) at {pixelRestingLocation}.");
+        }
+
+        /// <summary>
+        /// Occurs when the is released over the <see cref="MapPictureGrid"/>.
+        /// </summary>
+        /// <param name="inSender">The <see cref="MapPictureGrid"/> being hovered over.</param>
+        /// <param name="inMouseArguments">Arguments from the original event.</param>
+        /// <param name="clickStartLocation">The grid coordinates of the cell over which the click began.</param>
+        /// <param name="clickEndLocation">The grid coordinates of the cell over which the click ended.</param>
+        public void MapUp(object inSender, MouseEventArgs inMouseArguments, Point clickStartLocation, Point clickEndLocation)
+            => _ = MessageBox.Show($"Clicked starting at ({clickStartLocation.X}, {clickStartLocation.Y}) at ending at ({clickEndLocation.X}, {clickEndLocation.Y}).");
+        #endregion
     }
 }
